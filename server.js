c96ea491642714906c5aafab4331afe3cbf7147b
sara teshome <tes28sar@gmail.com>
2022-08-16 21:44:38 -0700

    trying to fix internal server error in Heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express")
+const path = require("path")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get("/" , (req, res) =>{
-    res.sendFile("./views/index.html")
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 } )  
 
 
