74dd383aca2414a45f4b83ad0f145361ec2ed04a
sara teshome <tes28sar@gmail.com>
2022-08-16 19:59:17 -0700

    starting a huroku project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/" , (req, res) =>{
+    res.send("hello world")
+} )  
+
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
+  
